63c1757c89412430266b95a602009d02f646a9bc
desiree25sfj <desiree25sfj@gmail.com>
2025-10-20 13:43:33 +0200

    add division

--- classes/Calculator.cs
+++ classes/Calculator.cs
@@ -1,6 +1,7 @@
 // main class (super class?), overloaded methods
 
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 public class Calculator
 {
@@ -50,5 +51,30 @@ public class Calculator
 			product *= num;
 		}
 		return product;
+	}
+
+	public double Divide(double a, double b)
+	{
+		if (b == 0)
+		{
+			throw new DivideByZeroException("Cannot divide by zero.");
+		}
+		return a / b;
+	}
+
+	public double Divide(List<double> numbers)
+	{
+		if (numbers.Count == 0) return 0;
+
+		double result = numbers[0];
+		for (int i = 1; i < numbers.Count; i++)
+		{
+			if (numbers[i] == 0)
+			{
+				throw new DivideByZeroException("Cannot divide by zero.");
+			}
+			result /= numbers[i];
+		}
+		return result;
 	}
 }
