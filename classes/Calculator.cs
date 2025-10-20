9cece1ae0c5df636cf45b46c8dc8433cb57506d1
desiree25sfj <desiree25sfj@gmail.com>
2025-10-20 13:36:19 +0200

    add addition hopefully

--- classes/Calculator.cs
+++ classes/Calculator.cs
@@ -1,1 +1,22 @@
 // main class (super class?), overloaded methods
+
+using System.Collections.Generic;
+
+public class Calculator
+{
+	public double Add(double a, double b)
+	{
+		return a + b;
+	}
+
+	public double Add(List<double> numbers)
+	{
+		double sum = 0;
+
+		foreach (double num in numbers)
+		{
+			sum += num;
+		}
+		return sum;
+	}
+}
