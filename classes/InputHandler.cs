943abee49308357b0ea03de7769968eadbdb8f30
desiree25sfj <desiree25sfj@gmail.com>
2025-10-20 13:55:42 +0200

    add input handler

--- classes/InputHandler.cs
+++ classes/InputHandler.cs
@@ -1,1 +1,25 @@
 // handle parsing and validation
+
+using System.Collections.Generic;
+
+public static class InputHandler
+{
+	public static List<double> ParseNumbers(string input)
+	{
+		List<double> numbers = new List<double>();
+		string[] parts = input.Split(' ', ',', StringSplitOptions.RemoveEmptyEntries);
+
+		foreach (string part in parts)
+		{
+			if (double.TryParse(part, out double num))
+			{
+				numbers.Add(num);
+			}
+			else
+			{
+				Console.WriteLine($"Warning: '{part}' is not a valid number and will be ignored.");
+			}
+		}
+		return numbers;
+	}
+}
