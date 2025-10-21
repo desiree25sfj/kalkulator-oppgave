d3a89446074d793a2f086267cf96e55f7e1e1c5b
desiree25sfj <desiree25sfj@gmail.com>
2025-10-21 12:44:34 +0200

    add aliases dictionary

--- classes/OperationDictionary.cs
+++ classes/OperationDictionary.cs
@@ -4,16 +4,38 @@ public static class OperationDictionary
 {
 	public static Dictionary<string, Func<List<double>, double>> GetOperations(Calculator calc)
 	{
-		return new Dictionary<string, Func<List<double>, double>>()
+		// return new Dictionary<string, Func<List<double>, double>>()
+		// {
+		// 	["add"] = nums => nums.Count == 2 ? calc.Add(nums[0], nums[1]) : calc.Add(nums),
+		// 	["subtract"] = nums => nums.Count == 2 ? calc.Subtract(nums[0], nums[1]) : calc.Subtract(nums),
+		// 	["multiply"] = nums => nums.Count == 2 ? calc.Multiply(nums[0], nums[1]) : calc.Multiply(nums),
+		// 	["divide"] = nums =>
+		// 	{
+		// 		if (nums.Contains(0) && nums.Count > 1) throw new DivideByZeroException("Cannot divide by zero.");
+		// 		return nums.Count == 2 ? calc.Divide(nums[0], nums[1]) : calc.Divide(nums);
+		// 	}
+		// };
+
+		var ops = new Dictionary<string, Func<List<double>, double>>(StringComparer.OrdinalIgnoreCase);
+
+		void AddWithAliases(string mainKey, Func<List<double>, double> func, params string[] aliases)
 		{
-			["add"] = nums => nums.Count == 2 ? calc.Add(nums[0], nums[1]) : calc.Add(nums),
-			["subtract"] = nums => nums.Count == 2 ? calc.Subtract(nums[0], nums[1]) : calc.Subtract(nums),
-			["multiply"] = nums => nums.Count == 2 ? calc.Multiply(nums[0], nums[1]) : calc.Multiply(nums),
-			["divide"] = nums =>
+			ops[mainKey] = func;
+			foreach (var alias in aliases)
 			{
-				if (nums.Contains(0) && nums.Count > 1) throw new DivideByZeroException("Cannot divide by zero.");
-				return nums.Count == 2 ? calc.Divide(nums[0], nums[1]) : calc.Divide(nums);
+				ops[alias] = func;
 			}
-		};
+		}
+
+		AddWithAliases("add", nums => nums.Count == 2 ? calc.Add(nums[0], nums[1]) : calc.Add(nums), "+", "sum", "plus");
+		AddWithAliases("subtract", nums => nums.Count == 2 ? calc.Subtract(nums[0], nums[1]) : calc.Subtract(nums), "-", "minus", "sub");
+		AddWithAliases("multiply", nums => nums.Count == 2 ? calc.Multiply(nums[0], nums[1]) : calc.Multiply(nums), "*", "x", "times");
+		AddWithAliases("divide", nums =>
+		{
+			if (nums.Contains(0) && nums.Count > 1) throw new DivideByZeroException("Cannot divide by zero.");
+			return nums.Count == 2 ? calc.Divide(nums[0], nums[1]) : calc.Divide(nums);
+		}, "/", ":", "divideby");
+
+		return ops;
 	}
 };
